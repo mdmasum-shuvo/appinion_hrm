a3739a4126fa9e1003dc5d74c875c2064aec1e9a
masumappinion <masum.taluklder@appinionbd.com>
2022-01-25 18:13:36 +0600

    dashboard and drawer design

--- lib/controller/AuthController.dart
+++ lib/controller/AuthController.dart
@@ -1,6 +1,5 @@
 import 'package:appinion_hrm/model/login_response.dart';
 import 'package:appinion_hrm/repository/AuthRepository.dart';
-import 'package:get/get_rx/src/rx_types/rx_types.dart';
 import 'package:get/state_manager.dart';
 
 class AuthController extends GetxController {
